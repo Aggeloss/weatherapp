079d8c3bdbf17083c9b83e794e653c9c2e69e048
Aggelos Gikas <aggelosgikas210@gmail.com>
2019-11-10 22:56:29 +0200

    pushh2

--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import { cityController } from './../modules/cityController';
-import { tempController } from './../modules/tempController';
 
 Vue.use(Vuex);
 
