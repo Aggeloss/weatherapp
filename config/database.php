5016763481ad1db42f08cb4c67808d21e562eb97
Aggelos Gikas <aggelosgikas210@gmail.com>
2019-11-10 23:11:37 +0200

    pushh3

--- config/database.php
+++ config/database.php
@@ -42,10 +42,13 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'http://weatherapplaravue.herokuapp.com/'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            // 'database' => env('DB_DATABASE', 'forge'),
+            'database' => env('DB_DATABASE', 'weather_app'),
+            // 'username' => env('DB_USERNAME', 'forge'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
